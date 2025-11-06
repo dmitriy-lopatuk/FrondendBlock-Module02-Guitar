dd21511bb949a37fc5904db3c87b675ae1e2d710
dmitriy-lopatuk <lopatyukdmitriy@mail.ru>
2025-11-06 17:22:04 +0600

    added swiper js to the project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-import './styles/main.scss'
+import 'normalize.css';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import './styles/main.scss';
+
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  modules: [Navigation, Pagination],
+});
